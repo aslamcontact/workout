0afdaa043950afa27e3e03b1827fcd5f27dc3184
aslamcontact <aslam.mycontact@gmail.com>
2024-01-28 21:18:00 +0530

    add interface for stack

---
+++ DSA/datastructure/src/main/java/aslam/mycontact/datastructure/linear/Stack.java
@@ -0,0 +1,10 @@
+package aslam.mycontact.datastructure.linear;
+
+public interface Stack {
+
+    void push(Integer value);
+    Integer pop();
+    Boolean isEmpty();
+    Boolean isFull();
+    Integer peek();
+}
