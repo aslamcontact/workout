2b67cfb80e6c302f1f7f2bc4ee64071eba908f6f
aslamcontact <aslam.mycontact@gmail.com>
2024-01-29 23:38:46 +0530

    implement the stack interface for simple stack

--- DSA/datastructure/src/main/java/aslam/mycontact/datastructure/DatastructureApplication.java
+++ DSA/datastructure/src/main/java/aslam/mycontact/datastructure/DatastructureApplication.java
@@ -1,7 +1,11 @@
 package aslam.mycontact.datastructure;
 
+import aslam.mycontact.datastructure.linear.Stack;
+import aslam.mycontact.datastructure.linear.implementation.stack.SimpleStack;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DatastructureApplication {
---
+++ DSA/datastructure/src/main/java/aslam/mycontact/datastructure/linear/implementation/stack/SimpleStack.java
@@ -0,0 +1,57 @@
+package aslam.mycontact.datastructure.linear.implementation.stack;
+
+import aslam.mycontact.datastructure.linear.Stack;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SimpleStack implements Stack {
+
+    private final List<Integer> items;
+    private final Integer size;
+    private Integer point=-1;
+
+    public SimpleStack(Integer size) {
+        this.items = new ArrayList<Integer>();
+        this.size = size;
+    }
+
+    @Override
+    public void push(Integer value) {
+        if(isFull())
+            throw  new IllegalStateException("Stack is full");
+        else{
+            items.add(value);
+            point++;
+        }
+
+    }
+
+    @Override
+    public Integer pop() {
+        Integer value;
+          if(isEmpty())
+             throw new IllegalStateException("Stack is empty");
+        value=items.remove(point.intValue());
+        point--;
+        return  value;
+    }
+
+    @Override
+    public Boolean isEmpty() {
+        return point==-1;
+    }
+
+    @Override
+    public Boolean isFull() {
+        return size-1==point;
+    }
+
+    @Override
+    public Integer peek() {
+
+        if(isEmpty()) throw new IllegalStateException("Stack is empty");
+        return items.get(point);
+    }
+}
