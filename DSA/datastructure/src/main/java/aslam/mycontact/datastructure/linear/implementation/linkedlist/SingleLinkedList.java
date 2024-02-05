e92af882bc1ab7af8b75af58a0e0af156fbc43a9
aslamcontact <aslam.mycontact@gmail.com>
2024-02-05 21:54:21 +0530

    implement linkedlist interfacer for single linked list

---
+++ DSA/datastructure/src/main/java/aslam/mycontact/datastructure/linear/implementation/linkedlist/SingleLinkedList.java
@@ -0,0 +1,13 @@
+package aslam.mycontact.datastructure.linear.implementation.linkedlist;
+
+import aslam.mycontact.datastructure.linear.implementation.linkedlist.node.Head;
+import aslam.mycontact.datastructure.linear.linkedlist.LinkedList;
+
+public class SingleLinkedList<T> implements LinkedList<T> {
+
+    private Head<T> head;
+    @Override
+    public Head<T> getHead() {
+        return null;
+    }
+}
