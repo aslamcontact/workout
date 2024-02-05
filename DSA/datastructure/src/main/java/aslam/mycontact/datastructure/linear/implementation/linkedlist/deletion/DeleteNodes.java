915c261e48228a3a6a1a9ef577613c4c7a56dce6
aslamcontact <aslam.mycontact@gmail.com>
2024-02-05 21:57:40 +0530

    implemt deletion class

---
+++ DSA/datastructure/src/main/java/aslam/mycontact/datastructure/linear/implementation/linkedlist/deletion/DeleteNodes.java
@@ -0,0 +1,20 @@
+package aslam.mycontact.datastructure.linear.implementation.linkedlist.deletion;
+
+import aslam.mycontact.datastructure.linear.linkedlist.Deletion;
+
+public class DeleteNodes<T> implements Deletion<T> {
+    @Override
+    public void removeFirst(T t) {
+
+    }
+
+    @Override
+    public void removeLast(T t) {
+
+    }
+
+    @Override
+    public void removeMiddle(T t) {
+
+    }
+}
