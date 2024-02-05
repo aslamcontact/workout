981448726835e217973acac0ec12bf57c5197317
aslamcontact <aslam.mycontact@gmail.com>
2024-02-05 22:00:10 +0530

    modify inserion interface to cofigure head

--- DSA/datastructure/src/main/java/aslam/mycontact/datastructure/linear/linkedlist/Insertion.java
+++ DSA/datastructure/src/main/java/aslam/mycontact/datastructure/linear/linkedlist/Insertion.java
@@ -1,10 +1,12 @@
 package aslam.mycontact.datastructure.linear.linkedlist;
 
+import aslam.mycontact.datastructure.linear.implementation.linkedlist.node.Head;
+import aslam.mycontact.datastructure.linear.linkedlist.nodeDesign.Node;
+
 public interface Insertion<T> {
 
 
-    void insertFirst(T t);
-    void insertLast(T t);
-    void insertMiddle(T t);
-
+    Head<T> insertFirst(T newNodeValue, Head<T> head);
+    Head<T> insertLast(T newNodeValue, Head<T> head);
+    Head<T> insertMiddle(T newNodeValue, Head<T> head, T addBefore);
 }
