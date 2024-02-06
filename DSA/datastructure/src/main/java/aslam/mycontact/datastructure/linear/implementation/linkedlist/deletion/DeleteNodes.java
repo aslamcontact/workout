d785b7b6ec3cca757aa99c252ede6edadc94cdc6
aslamcontact <aslam.mycontact@gmail.com>
2024-02-06 15:11:21 +0530

    implement deletion logic using deletion interface

--- DSA/datastructure/src/main/java/aslam/mycontact/datastructure/linear/implementation/linkedlist/deletion/DeleteNodes.java
+++ DSA/datastructure/src/main/java/aslam/mycontact/datastructure/linear/implementation/linkedlist/deletion/DeleteNodes.java
@@ -1,20 +1,36 @@
 package aslam.mycontact.datastructure.linear.implementation.linkedlist.deletion;
 
+import aslam.mycontact.datastructure.linear.implementation.linkedlist.node.Head;
 import aslam.mycontact.datastructure.linear.linkedlist.Deletion;
+import aslam.mycontact.datastructure.linear.linkedlist.nodeDesign.Node;
 
 public class DeleteNodes<T> implements Deletion<T> {
     @Override
-    public void removeFirst(T t) {
-
+    public Head<T> removeFirst(Head<T> head) {
+        if(head.getNode()==null) throw new IllegalStateException("Empty Node");
+        head.setNode(head.getNode().nextNode());
+        return head;
     }
 
     @Override
-    public void removeLast(T t) {
-
+    public Head<T> removeLast(Head<T> head) {
+        Node<T> currentNode=head.getNode();
+        if(currentNode==null) throw new IllegalStateException("Empty Node");
+        while (currentNode.nextNode()!=null)
+        {
+            if(currentNode.nextNode().nextNode()==null)
+            {
+                currentNode.setNextNode(null);
+                break;
+            }
+            currentNode=currentNode.nextNode();
+        }
+        return  head;
     }
 
     @Override
-    public void removeMiddle(T t) {
+    public Head<T> removeMiddle(T t, Head<T> head) {
 
+        return null;
     }
 }
