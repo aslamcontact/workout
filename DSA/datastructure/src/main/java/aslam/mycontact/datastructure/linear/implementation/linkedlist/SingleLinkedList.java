b028be9abec3c54e6a7e5010ae79418eb5607828
aslamcontact <aslam.mycontact@gmail.com>
2024-02-06 18:54:35 +0530

    implements linkedIn methods addfirst and addLast

--- DSA/datastructure/src/main/java/aslam/mycontact/datastructure/linear/implementation/linkedlist/SingleLinkedList.java
+++ DSA/datastructure/src/main/java/aslam/mycontact/datastructure/linear/implementation/linkedlist/SingleLinkedList.java
@@ -1,13 +1,35 @@
 package aslam.mycontact.datastructure.linear.implementation.linkedlist;
 
+import aslam.mycontact.datastructure.linear.implementation.linkedlist.insertions.InsertNodes;
 import aslam.mycontact.datastructure.linear.implementation.linkedlist.node.Head;
+import aslam.mycontact.datastructure.linear.implementation.linkedlist.node.SingleNode;
+import aslam.mycontact.datastructure.linear.linkedlist.Insertion;
 import aslam.mycontact.datastructure.linear.linkedlist.LinkedList;
+import aslam.mycontact.datastructure.linear.linkedlist.nodeDesign.Node;
 
 public class SingleLinkedList<T> implements LinkedList<T> {
-
+    Insertion<T> insertion;
     private Head<T> head;
+
+    public SingleLinkedList() {
+        this.head=new Head<T>();
+        this.insertion= new InsertNodes<T>();
+    }
+
+
     @Override
     public Head<T> getHead() {
-        return null;
+        return head;
     }
+    public void addFirst(T value)
+    {
+       head=insertion.insertFirst(value,head);
+    }
+    public void addLast(T value)
+    {
+
+        head=insertion.insertLast(value,head);
+    }
+
+
 }
