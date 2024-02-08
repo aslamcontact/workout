2896bda1f1770e42a7801da3a904e843651bfc15
aslamcontact <aslam.mycontact@gmail.com>
2024-02-08 20:35:01 +0530

    implements deletion on singleLinkedList

--- DSA/datastructure/src/main/java/aslam/mycontact/datastructure/linear/implementation/linkedlist/SingleLinkedList.java
+++ DSA/datastructure/src/main/java/aslam/mycontact/datastructure/linear/implementation/linkedlist/SingleLinkedList.java
@@ -1,18 +1,23 @@
 package aslam.mycontact.datastructure.linear.implementation.linkedlist;
 
+import aslam.mycontact.datastructure.linear.implementation.linkedlist.operations.deletion.DeleteNodes;
 import aslam.mycontact.datastructure.linear.implementation.linkedlist.operations.insertions.InsertNodes;
 import aslam.mycontact.datastructure.linear.implementation.linkedlist.node.Head;
 import aslam.mycontact.datastructure.linear.linkedlist.AddElement;
+import aslam.mycontact.datastructure.linear.linkedlist.RemoveElement;
+import aslam.mycontact.datastructure.linear.linkedlist.operations.Deletion;
 import aslam.mycontact.datastructure.linear.linkedlist.operations.Insertion;
 import aslam.mycontact.datastructure.linear.linkedlist.LinkedList;
 
-public class SingleLinkedList<T> implements LinkedList<T>, AddElement<T> {
+public class SingleLinkedList<T> implements LinkedList<T>, AddElement<T>, RemoveElement<T> {
     Insertion<T> insertion;
+    Deletion<T> deletion;
     private Head<T> head;
 
     public SingleLinkedList() {
         this.head=new Head<T>();
         this.insertion= new InsertNodes<T>();
+        this.deletion=new DeleteNodes<>();
     }
 
 
@@ -31,4 +36,13 @@ public class SingleLinkedList<T> implements LinkedList<T>, AddElement<T> {
     }
 
 
+    @Override
+    public void removeFirst() {
+          deletion.removeFirst(this.head);
+    }
+
+    @Override
+    public void removeLast() {
+        deletion.removeLast(this.head);
+    }
 }
